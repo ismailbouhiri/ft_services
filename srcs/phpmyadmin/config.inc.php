ae39933e223831e7f2b5496c0413c547b9abc7be
Ismail Bouhiri <ibouhiri@e2r9p8.1337.ma>
2021-01-26 08:49:03 +0100

    direction 307

--- srcs/phpmyadmin/config.inc.php
+++ srcs/phpmyadmin/config.inc.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = 'wiii3'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = '07bELTT}eb1F[2W}QoCuBF;t2]QV]J0b'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
